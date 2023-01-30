c2903859e41fe6f92b8c9c3ee8e7afda85144a6f
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2023-01-30 16:07:04 +0000

    Store round objects as list and refactor code accordingly, Store Round points in a list, Add choice points to point calculation

--- d1/d1.py
+++ d1/d1.py
@@ -1,31 +1,34 @@
 class RPS:
     """Represents a game of Rock, Paper, Scissors."""
+
     def __init__(self, inputs):
         self.inputs = self.process_inputs(inputs)
-        self.p1_round_points = []
-        self.p2_round_points = []
+        self.rounds = []  # List of played rounds
 
     @property
     def p1_score(self):
-        return sum(self.p1_round_points)
+        """Calculates score for p1 from list of Rounds."""
+        scores = [r.points[0] for r in self.rounds]
+        return sum(scores)
 
     @property
     def p2_score(self):
-        return sum(self.p2_round_points)
+        """Calculates score for p2 from list of Rounds."""
+        scores = [r.points[1] for r in self.rounds]
+        return sum(scores)
 
     def process_inputs(self, inputs):
         """Process file of inputs into a list of tuples.
         Translate inputs to use same coding for same inputs."""
 
-        # Initialize translations to uniform input coding
-        # X: rock, Y: paper, Z: scissors
+        # Initialize translations to uniform input coding: XYZ -> ABC
+        # A: rock, B: paper, C: scissors
         trantab = str.maketrans("XYZ", "ABC")
 
         with open(f"d1/{inputs}", "r", encoding="UTF-8") as f:
             inputs_processed = []
 
             for line in f.readlines():
-                # Translate XYZ --> ABC
                 line_translated = line.translate(trantab)
 
                 # Convert str to tuples, combine to a list of tuples
@@ -38,53 +41,67 @@ class RPS:
     def play(self):
         """Play a game of Rock Paper Scissors. Player choices given in inputs."""
 
-        # Loop through a list of tuples holding player choices 
-        # for individual Rounds
+        # Loop through a list of tuples holding player choices for individual Rounds
         for choices in self.inputs:
             new_round = Round(choices)
-            outcome = new_round.calculate_outcome()
-            self.p1_round_points.append(outcome[0])
-            self.p2_round_points.append(outcome[1])
-
-        # print(self.p1_scores)
+            new_round.calculate_outcome()
+            self.rounds.append(new_round)
 
 
 class Round:
     """
-        Represents a single round in a game of Rock, Paper, Scissors.
-        A Round can end in p1 winning, p2 winning, or a draw.
+    Represents a single round in a game of Rock, Paper, Scissors.
+    A Round can end in p1 winning, p2 winning, or a draw.
     """
+
+    # Points given for the choice made in a Round
+    # A -> Rock, B -> Paper, C -> Scissors
+    dict_choice_points = {"A": 1, "B": 2, "C": 3}
+
     def __init__(self, choices):
         self.choices = choices
-        self.points = (0, 0)
+        self.points = self.calculate_choice_points()
 
-    def calculate_outcome(self):
-        """Calculates the outcome of the game."""
+    def calculate_choice_points(self):
+        """Calculates the choice points in accordance of dict_choice_points."""
 
+        # Ensure choices are consistent with expected ABC coding
         if self.choices[0] not in ("A", "B", "C") or self.choices[1] not in (
             "A",
             "B",
             "C",
         ):
-            return f"invalid inputs: {self.choices[0]}, {self.choices[1]}"
+            print(f"invalid inputs: {self.choices[0]}, {self.choices[1]}")
+
+        choice_points = [self.dict_choice_points[choice] for choice in self.choices]
+
+        return choice_points
+
+    def calculate_outcome(self):
+        """Calculates the outcome of the game."""
 
         if self.choices[0] == self.choices[1]:
-            return (3, 3)
+            self.points[0] += 3
+            self.points[1] += 3
+            return self
 
         p1_wins = [("A", "C"), ("B", "A"), ("C", "B")]
 
         if self.choices in (p1_wins):
-            return (6, 0)
+            self.points[0] += 6
+            return self
         else:
-            return (0, 6)
+            self.points[1] += 6
+            return self
 
 
 def main():
     game = RPS("inputs.txt")  # A: rock VS. Y: scissors --> Y: scissors wins
     game.play()
 
-    print(game.p1_score)
-    print(game.p2_score)
+    print('Total scores after tournament')
+    print(f'My score: {game.p1_score}')
+    print(f'Opponents score: {game.p2_score}')
 
 
 if __name__ == "__main__":
