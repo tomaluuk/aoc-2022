f05dd9e86088a5bd93d79eb366c2664ec7215a53
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2023-01-30 09:01:52 +0000

    Add Test File

---
+++ d1/testing.py
@@ -0,0 +1,8 @@
+with open('d1/inputs_sample.txt', 'r') as f:
+    inputs_sample = []
+    for line in f.readlines():
+        line_trimmed_and_splitted = line.strip('\n').split(' ')
+        tuple_line_trimmed = tuple(line_trimmed_and_splitted)
+        inputs_sample.append()
+
+print(inputs_sample)
