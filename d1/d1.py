cb4f3d1c73bc43e09e36d7503939e05f8b3f5c43
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2023-01-30 10:43:13 +0000

    Add self references, fix Round init, remove manual inputs

--- d1/d1.py
+++ d1/d1.py
@@ -6,13 +6,13 @@ class RPS:
         """Process file of inputs into a list of tuples.
         Translate inputs to use same coding for same inputs."""
 
-        # Initialize translations to uniform input coding 
+        # Initialize translations to uniform input coding
         # X: rock, Y: paper, Z: scissors
         trantab = str.maketrans("XYZ", "ABC")
 
         with open(f"d1/{inputs}", "r", encoding="UTF-8") as f:
             inputs_processed = []
-    
+
             for line in f.readlines():
                 # Translate XYZ --> ABC
                 line_translated = line.translate(trantab)
@@ -30,45 +30,41 @@ class RPS:
         results = []
 
         for choices in self.inputs:
-            new_round = Round()
-            outcome = new_round.calculate_outcome(choices)
+            new_round = Round(choices)
+            outcome = new_round.calculate_outcome()
             results.append(outcome)
 
         print(results)
 
 
 class Round:
-    def calculate_outcome(self, choices):
+    def __init__(self, choices):
+        self.choices = choices
+
+    def calculate_outcome(self):
         """Calculates the outcome of the game."""
         # {'Rock': 'A', 'Paper': 'B', 'Scissors': 'C'}
         # {'Rock': 'X', 'Paper': 'Y', 'Scissors': 'Z'}
-        def __init__(self, choices):
-            self.choices = choices
 
-        if choices[0] not in ("A", "B", "C") or choices[1] not in (
-            "X",
-            "Y",
-            "Z",
+        if self.choices[0] not in ("A", "B", "C") or self.choices[1] not in (
+            "A",
+            "B",
+            "C",
         ):
-            return f"invalid inputs: {choices[0]}, {choices[1]}"
+            return f"invalid inputs: {self.choices[0]}, {self.choices[1]}"
 
-        if choices[0] == choices[1]:
-            return "draw: {}"
+        if self.choices[0] == self.choices[1]:
+            return "draw"
 
-        p1_wins = [("A", "Z"), ("B", "X"), ("C," "Y")]
+        p1_wins = [("A", "C"), ("B", "A"), ("C", "B")]
 
-        if choices in (p1_wins):
+        if self.choices in (p1_wins):
             return "p1 wins"
         else:
             return "p2 wins"
 
 
 def main():
-    # with open('inputs.txt', 'r') as f:
-    #    f.readline()
-    inputs_text = "A Y\nB T\nC Z"
-    inputs = [("A", "Y"), ("B", "T"), ("C", "Z")]
-
     game = RPS("inputs.txt")  # A: rock VS. Y: scissors --> Y: scissors wins
     game.play()
 
